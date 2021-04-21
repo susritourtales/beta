2e488b0355672f6802fdc3f44cb8a6614ab9ff48
susritourtales <80307181+susritourtales@users.noreply.github.com>
2021-04-21 16:05:54 +0530

    Update github.php

--- github.php
+++ github.php
@@ -1,7 +1,7 @@
 <?php //`git pull`; ?>
 <?php
 
-$output = exec('cd /var/www/html/public/beta && /usr/bin/git pull');
+$output = shell_exec('cd /var/www/html/public/beta && /usr/bin/git pull 2>&1');
   
 echo "<pre>$output</pre>";
 
