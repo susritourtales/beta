f1ac106a09cb16ae72bdc2e72b4e19cf6e2bc667
susritourtales <80307181+susritourtales@users.noreply.github.com>
2021-04-21 15:46:26 +0530

    Update github.php

--- github.php
+++ github.php
@@ -1,1 +1,11 @@
-<?php `git pull`;
+<?php //`git pull`; ?>
+?php
+  
+// Use ls command to shell_exec
+// function
+$output = shell_exec('git pull');
+  
+// Display the list of all file
+// and directory
+echo "<pre>$output</pre>";
+?>
