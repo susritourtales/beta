6c775e2cb81970c6c05ac611801c48a277c6e845
susritourtales <80307181+susritourtales@users.noreply.github.com>
2021-04-21 15:54:28 +0530

    Update github.php

--- github.php
+++ github.php
@@ -1,29 +1,14 @@
 <?php //`git pull`; ?>
 <?php
+ 
+$output = exec('git pull');
   
-// Use ls command to shell_exec
-// function
-//$output = shell_exec('git pull');
-  
-// Display the list of all file
-// and directory
-//echo "<pre>$output</pre>";
+echo "<pre>$output</pre>";
 
 //Store the output of the executed command
 
 $output1 =  exec('pwd');
 
 //Print the return value
-
 echo $output1;
-
-echo "<br/>";
-
-//Store the last line of the executed command
-
-$output2 = exec('ls -la');
-
-//Print the return value
-
-echo $output2;
 ?>
