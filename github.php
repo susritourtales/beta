da43bb530efec67ee23858a227e03834cbd2d659
susritourtales <80307181+susritourtales@users.noreply.github.com>
2021-04-22 08:57:09 +0530

    Update github.php

--- github.php
+++ github.php
@@ -1,14 +1,5 @@
 <?php //`git pull`; ?>
-<?php
-echo exec('whoami'); 
+<?php 
 $output = shell_exec('sudo git pull https://github.com/susritourtales/beta.git');
-  
 echo "<pre>$output</pre>";
-
-//Store the output of the executed command
-
-$output1 =  exec('pwd');
-
-//Print the return value
-echo $output1;
 ?>
